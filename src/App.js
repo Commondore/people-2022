4cdb2a8d438066de37df5d461989943b14c6d8ba
Alex Som <alex.som86@gmail.com>
2023-01-31 14:40:46 +0600

    add state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,65 @@
 import Person from "components/Person/Person";
 import "./App.css";
 
+import { useState } from "react";
+
 function App() {
+  const [people, setPeople] = useState([
+    { name: "Mike", age: 30, prof: "Программист" },
+    { name: "Sam", age: 20, prof: "UI/UX Designer" },
+  ]);
+
+  const [title, setTitle] = useState("Hello React");
+
+  const changeName = () => {
+    // const person = { ...people[0] };
+    // person.name = "Ketty";
+    // const copyPeople = [...people];
+    // copyPeople[0] = person;
+
+    // setPeople(copyPeople);
+
+    setPeople(() => {
+      return people.map((person, index) => {
+        if (index === 1) {
+          person.name = "Ketty";
+        }
+        return person;
+      });
+    });
+  };
+
+  const changeTitle = () => {
+    setTitle("New title");
+  };
+
+  const increaseAge = () => {
+    const copy = people.map((person) => {
+      //{ name: "Mike", age: 30, prof: "Программист" }
+      return {
+        ...person,
+        age: person.age + 1,
+      };
+    });
+    setPeople(copy);
+  };
+
   return (
     <div className="App">
-      <h1>People list</h1>
+      <h1 onClick={changeTitle}>{title}</h1>
+      <button className="btn" onClick={changeName}>
+        Change Name
+      </button>
+      <button className="btn" onClick={increaseAge}>
+        Увеличить возраст
+      </button>
       <div className="people">
-        <Person name="Sam" age="25" />
-        <Person name="Mike" age="35" />
+        <Person name={people[0].name} age={people[0].age}>
+          {people[0].prof}
+        </Person>
+        <Person name={people[1].name} age={people[1].age}>
+          {people[1].prof}
+        </Person>
       </div>
     </div>
   );
--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,12 +1,15 @@
 import "./Person.css";
-// import ProfileImg from './profile.png';
 
-const Person = function (props) {
-  // const {name, age} = props;
+const Person = (props) => {
   return (
     <div className="person">
       <h3 className="title">{props.name}</h3>
-      <p className="age">Age: {props.age}</p>
+      <p className="age">
+        Age: <span>{props.age}</span>
+      </p>
+      <p className="prof">
+        Профессия: <b>{props.children}</b>
+      </p>
     </div>
   );
 };
